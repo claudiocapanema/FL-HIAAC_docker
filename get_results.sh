d34687a1cd902a65e7d88836bf705be32acc2d4d
claudiocapanema <claudiogs.capanema@gmail.com>
2025-02-10 10:22:12 -0300

    Generating train and test metrics

---
+++ get_results.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Nome do contêiner que você quer buscar
+CONTAINER_NAME="fl-hiaac_docker-server"
+
+# Diretório dentro do contêiner que você quer copiar
+CONTAINER_DIR=$/results
+
+# Diretório de destino na máquina local
+LOCAL_DIR=$/home/claudio/PycharmProjects/FL-HIAAC_docker/
+
+# Verifica se todos os argumentos foram passados
+if [ -z "$CONTAINER_NAME" ] || [ -z "$CONTAINER_DIR" ] || [ -z "$LOCAL_DIR" ]; then
+    echo "Uso: $0 <nome_do_container> <diretorio_no_container> <diretorio_local>"
+    exit 1
+fi
+
+# Obtém o ID do contêiner com base no nome
+CONTAINER_ID=$(docker ps -q -f "ancestor=$CONTAINER_NAME")
+
+# Verifica se o contêiner foi encontrado
+if [ -z "$CONTAINER_ID" ]; then
+    echo "Erro: Contêiner com nome '$CONTAINER_NAME' não encontrado."
+    exit 1
+fi
+
+echo "ID do contêiner encontrado: $CONTAINER_ID"
+
+# Copia a pasta do contêiner para a máquina local
+docker cp "$CONTAINER_ID:$CONTAINER_DIR" "$LOCAL_DIR"
+
+chmod 777 "$LOCAL_DIR"
+
+# Verifica se a operação de cópia foi bem-sucedida
+if [ $? -eq 0 ]; then
+    echo "Pasta copiada com sucesso de '$CONTAINER_DIR' para '$LOCAL_DIR'"
+else
+    echo "Erro ao copiar a pasta."
+fi
