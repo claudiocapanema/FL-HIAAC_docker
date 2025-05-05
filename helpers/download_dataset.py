50ef34afe9fb924a24e33dcc46c37093c5af5025
claudiogsc <claudiogs.capanema@gmail.com>
2025-05-05 11:44:06 -0300

    Dataset from server (volume)

--- helpers/download_dataset.py
+++ helpers/download_dataset.py
@@ -15,21 +15,24 @@ def download_datasets(datasets_name: list, alphas: list, num_partitions: int):
         logger.info(
             """Loading {} {} {} data.""".format(datasets_name, num_partitions, alphas))
         pasta = './datasets/'
-        for arquivo in os.listdir(pasta):
-            caminho_completo = os.path.join(pasta, arquivo)
-            if os.path.isfile(caminho_completo):
-                os.remove(caminho_completo)
-                logger.info("Removed {}".format(caminho_completo))
+        # for arquivo in os.listdir(pasta):
+        #     caminho_completo = os.path.join(pasta, arquivo)
+        #     if os.path.isfile(caminho_completo):
+        #         os.remove(caminho_completo)
+        #         logger.info("Removed {}".format(caminho_completo))
         global fds
         for i in range(len(datasets_name)):
             dataset_name = datasets_name[i]
-            if dataset_name not in fds:
+            filename = f"datasets/{dataset_name}"
+            if dataset_name not in fds and not os.path.isdir(filename):
+                logger.info("Downloading {}".format(dataset_name))
                 dataset = load_dataset({"EMNIST": "claudiogsc/emnist_balanced", "CIFAR10": "uoft-cs/cifar10", "MNIST": "ylecun/mnist",
                              "GTSRB": "claudiogsc/GTSRB", "Gowalla": "claudiogsc/Gowalla-State-of-Texas",
                              "WISDM-W": "claudiogsc/WISDM-W", "ImageNet": "claudiogsc/ImageNet-15_household_objects"}[
                         dataset_name])
-                dataset.save_to_disk(f"datasets/{dataset_name}")
-
+                dataset.save_to_disk(filename)
+            else:
+                logger.info("Found {}".format(dataset_name))
 
     except Exception as e:
         logger.error("load_data error")
